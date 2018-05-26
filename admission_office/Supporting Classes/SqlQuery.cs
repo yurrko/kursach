cade4c1ab6acff9487515daeccd04ca9ea812c33
yurrko <yurrko@yandex.ru>
2018-05-26 14:05:34 +0300

    Добавлены новые SQL запросы для взаимодействия с драйвером БД.

--- admission_office/Supporting Classes/SqlQuery.cs
+++ admission_office/Supporting Classes/SqlQuery.cs
@@ -1,8 +1,8 @@
 ﻿namespace admission_office
 {
-    class SqlQuery
+    class SqlQueryList
     {
-        public static string[] SqlQueries { get; } = new string[]
+        public static string[] Queries { get; } = new string[]
         {
             @"SELECT e.id, CONCAT(speciality,' (',education_form,')') as Edu FROM admission_office.education e 
                 INNER JOIN admission_office.speciality s ON e.id_speciality = s.id
@@ -10,7 +10,11 @@
                 ORDER BY e.id",
             "SELECT id, subject FROM admission_office.subject ORDER BY id",
             "SELECT id FROM admission_office.speciality WHERE speciality = @speciality",
-            "SELECT * FROM admission_office.report"
+            "SELECT * FROM admission_office.report",
+            "INSERT INTO `admission_office`.`entrants` (`first_name`, `middle_name`, `last_name`, `birth_date`) VALUES (@0, @1, @2, @3)",
+            "SELECT role FROM admission_office.authorize WHERE Login = @0 AND password = @1",
+            "INSERT INTO `admission_office`.`log` (`timestamp`, `user`, `success`) VALUES (@0, @1, @2)",
+            "INSERT INTO admission_office.authorize (login, password, role) VALUES (@0, @1, @2)"
     };
     }
     enum SqlQueryNum
@@ -18,6 +22,10 @@
         EduSpec,
         Subject,
         SpecId,
-        Report
+        Report,
+        AddEntrant,
+        SelectRole,
+        Log,
+        Register
     }
 }
