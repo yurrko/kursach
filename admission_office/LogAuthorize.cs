84b9bd6a806cc036791b58a103ff34c041def51b
yurrko <yurrko@yandex.ru>
2018-05-26 14:05:03 +0300

    Переписаны методы класс Log_authorize для работы через новый драйвер обращений к БД

--- admission_office/LogAuthorize.cs
+++ admission_office/LogAuthorize.cs
@@ -9,100 +9,57 @@ namespace admission_office
 {
     class LogAuthorize
     {
-        MD5 _md5Hash;
-        public int Authorize( string login, string password)
-        {
-            _md5Hash = MD5.Create();
-            string result;
-            using (MySqlConnection connection = new MySqlConnection( ConnectionString.Connection ))
-            {
-                MySqlCommand cmd = new MySqlCommand();
-                connection.Open();
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = connection;
-                cmd.CommandText = "SELECT role FROM authorize WHERE Login = @Login AND password = @Password";
-                cmd.Parameters.AddWithValue( "@Login", login );
-                cmd.Parameters.AddWithValue( "@Password", Encrypt( _md5Hash, password ) );
-                try {
-                    result = cmd.ExecuteScalar().ToString();
-                }
-                catch (Exception ex)
-                {
-                    Log( login, false );
-                    return -1;
-                }
-                connection.Close();
-            }
-            Log( login, true );
-            return int.Parse(result);
-        }
+        private static LogAuthorize _instance;
 
-        private void Log( string login, bool result)
+        public static LogAuthorize Instance
         {
-            using (MySqlConnection connection = new MySqlConnection( ConnectionString.Connection ))
+            get
             {
-                MySqlCommand cmd = new MySqlCommand();
-                connection.Open();
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = connection;
-                cmd.CommandText = "INSERT INTO `admission_office`.`log` (`timestamp`, `user`, `success`) VALUES (@timeStamp, @user, @success)";
-                var dt = DateTime.Now;
-                cmd.Parameters.AddWithValue( "@timeStamp", dt );
-                cmd.Parameters.AddWithValue( "@user", login );
-                cmd.Parameters.AddWithValue( "@success", result);
-                cmd.ExecuteNonQuery();
-                connection.Close();
+                if (_instance == null) _instance = new LogAuthorize();
+                return _instance;
             }
         }
 
-        public bool Register( string login, string password, int role )
+        public int Authorize( string login, string password, int DBNum )
         {
-            _md5Hash = MD5.Create();
-            using (MySqlConnection connection = new MySqlConnection( ConnectionString.Connection))
+            using (MD5 md5Hash = MD5.Create())
             {
-                connection.Open();
-                MySqlCommand cmd = connection.CreateCommand();
-                cmd.CommandType = CommandType.Text;
-                cmd.CommandText = "INSERT INTO authorize (login, password, role) VALUES (@Login, @Password, @Role)";
-                cmd.Parameters.Clear();
-                cmd.Parameters.AddWithValue( "@Login", login );
-                cmd.Parameters.AddWithValue( "@Password", Encrypt( _md5Hash, password ) );
-                cmd.Parameters.AddWithValue( "@Role", role );
-                var result = 0;
-                try
-                {
-                    result = cmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                    return false;
-                }
-                connection.Close();
-                if (result == 1) MessageBox.Show("Пользователь зарегистрирован", "Сообщение");
+                DBDriver.ConnectionNum = DBNum;
+                int result = DBDriver.Instance.SelectOneValue( SqlQueryList.Queries[(int)SqlQueryNum.SelectRole],
+                                                                new string[] { login, Encrypt( md5Hash, password ) } );
+                if (result == -1)
+                    Log( login, "0" );
+                else
+                    Log( login, "1" );
+                return result;
             }
-            return true;
+        }
+
+        public bool Register( string login, string password, string role )
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                return DBDriver.Instance.InsertValues( SqlQueryList.Queries[(int)SqlQueryNum.Register],
+                                                    new string[] { login, Encrypt( md5Hash, password ), role } );
+            }
+        }
+
+        private void Log( string login, string result )
+        {
+            var date = String.Format( "{0:s}", DateTime.Now );
+            DBDriver.Instance.InsertValues( SqlQueryList.Queries[(int)SqlQueryNum.Log],
+                                            new string[] { date, login, result } );
         }
 
         private string Encrypt( MD5 md5Hash, string input )
         {
-            // Convert the input string to a byte array and compute the hash.
             byte[] data = md5Hash.ComputeHash( Encoding.UTF8.GetBytes( input ) );
-
-            // Create a new Stringbuilder to collect the bytes
-            // and create a string.
             StringBuilder sBuilder = new StringBuilder();
-
-            // Loop through each byte of the hashed data 
-            // and format each one as a hexadecimal string.
             for (int i = 0; i < data.Length; i++)
-            {
                 sBuilder.Append( data[i].ToString( "x2" ) );
-            }
-
-            // Return the hexadecimal string.
             return sBuilder.ToString();
         }
     }
 }
+
 
