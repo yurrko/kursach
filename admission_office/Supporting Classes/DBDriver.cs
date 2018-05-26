d8a0b2c218422c1769e2ea86a7cdf5a8549006e6
yurrko <yurrko@yandex.ru>
2018-05-26 23:53:39 +0300

    Новая функция в DBDriver для получения id только вставленной записи

--- admission_office/Supporting Classes/DBDriver.cs
+++ admission_office/Supporting Classes/DBDriver.cs
@@ -21,6 +21,14 @@ namespace admission_office
         private readonly string[] _connStr = { "server=localhost;user=root;database=admission_office;password=12345687",
                                         "server=localhost;user=root;database=admission_office_archive;password=12345687" };
 
+        /*Начало. Удалить после полного переписования AOffice*/
+        public string Connect
+
+        {
+            get { return _connStr[0]; }
+        }
+        /*Конец. Удалить после полного переписования AOffice*/
+
         public static int ConnectionNum { get; set; }
 
         public int SelectOneValue( string sql, string[] values )
@@ -74,6 +82,35 @@ namespace admission_office
                 }
                 connection.Close();
                 return true;
+            }
+        }
+
+        public int InsertValuesAndReceiveIdentity( string sql, string[] values )
+        {
+            using (MySqlConnection connection = new MySqlConnection( _connStr[ConnectionNum] ))
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                connection.Open();
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = connection;
+                cmd.CommandText = sql;
+                for (int i = 0; i < values.Length; i++)
+                {
+                    cmd.Parameters.AddWithValue( String.Concat( "@", i ), values[i] );
+                }
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show( ex.Message, "Ошибка" );
+                    //return false;
+                }
+                cmd.CommandText = "SELECT @@IDENTITY";
+                int lastId = Convert.ToInt32( cmd.ExecuteScalar() );
+                connection.Close();
+                return lastId;
             }
         }
     }
