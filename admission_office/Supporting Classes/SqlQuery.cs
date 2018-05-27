0ecd72256324f578806af7d061defdd08c107c77
yurrko <yurrko@yandex.ru>
2018-05-27 11:28:47 +0300

    Новые запросы для AOffice исправление ошибок с параметрами для запросов (@speciality --> @0)

--- admission_office/Supporting Classes/SqlQuery.cs
+++ admission_office/Supporting Classes/SqlQuery.cs
@@ -9,7 +9,7 @@
                 INNER JOIN admission_office.education_form ef ON e.id_education_form = ef.id
                 ORDER BY e.id",
             "SELECT id, subject FROM `admission_office`.`subject` ORDER BY `id`",
-            "SELECT id FROM `admission_office`.`speciality` WHERE `speciality` = @speciality",
+            "SELECT id FROM `admission_office`.`speciality` WHERE `speciality` = @0",
             "SELECT * FROM `admission_office`.`report`",
             "INSERT INTO `admission_office`.`entrants` (`first_name`, `middle_name`, `last_name`, `birth_date`) VALUES (@0, @1, @2, @3)",
             "SELECT `role` FROM `admission_office`.`authorize` WHERE `login` = @0 AND `password` = @1",
@@ -17,7 +17,10 @@
             "INSERT INTO `admission_office`.`authorize` (`login`, `password`, `role`) VALUES (@0, @1, @2)",
             "INSERT INTO `admission_office`.`ege_result` (`id_entrant`, `id_subject`, `result`) VALUES (@0, @1, @2)",
             "INSERT INTO `admission_office`.`entrance` (`id_entrant`, `id_education`, `date`) VALUES (@0, @1, @2)",
-            "INSERT INTO `admission_office`.`subject` (`subject`) VALUES (@subject)"
+            "INSERT INTO `admission_office`.`subject` (`subject`) VALUES (@0)",
+            "INSERT INTO `admission_office`.`speciality` (`speciality`) VALUES( @0 )",
+            "INSERT INTO `admission_office`.`education` (`id_speciality`, `id_education_form`, `num_of_free_places`, `num_of_paid_places`) VALUES( @0, @1, @2, @3)",
+            "INSERT INTO `admission_office`.`requirement` (`id_education`, `id_subject`, `min_requirement`) VALUES (@0, @1, @2)"
     };
     }
     enum SqlQueryNum
@@ -32,6 +35,9 @@
         Register,
         AddEgeResult,
         AddEntrance,
-        AddSubject
+        AddSubject,
+        AddSpeciality,
+        AddEducation,
+        AddRequirement
     }
 }
