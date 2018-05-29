5db59e16e7dcbe8e48935711fea765838e8e4c27
yurrko <yurrko@yandex.ru>
2018-05-29 16:15:26 +0300

    Новый usercontrol - таблица

---
+++ admission_office/MyUserControl/TestGrid.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace admission_office
+{
+    partial class TestGrid
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEntrants = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEntrants)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEntrants
+            // 
+            this.dgvEntrants.AllowUserToOrderColumns = true;
+            this.dgvEntrants.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEntrants.Location = new System.Drawing.Point(3, 3);
+            this.dgvEntrants.Name = "dgvEntrants";
+            this.dgvEntrants.RowTemplate.Height = 24;
+            this.dgvEntrants.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvEntrants.Size = new System.Drawing.Size(988, 526);
+            this.dgvEntrants.TabIndex = 0;
+            // 
+            // TestGrid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgvEntrants);
+            this.Name = "TestGrid";
+            this.Size = new System.Drawing.Size(994, 532);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEntrants)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEntrants;
+    }
+}
---
+++ admission_office/MyUserControl/TestGrid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace admission_office
+{
+    public partial class TestGrid : UserControl
+    {
+        private static TestGrid _instance;
+
+        public static TestGrid Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new TestGrid();
+                return _instance;
+            }
+        }
+
+        private TestGrid()
+        {
+            InitializeComponent();
+            var ds = new DataSet();
+            ds = DBDriver.Instance.SelectValuesDataSet(SqlQueryList.Queries[(int) SqlQueryNum.Report]);
+            dgvEntrants.DataSource = ds.Tables[0];
+        }
+    }
+}
